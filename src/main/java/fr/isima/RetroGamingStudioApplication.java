6fecb159a1e281031182cade97772b010a796306
USSON Julien <juusson2@rcisima.isima.fr>
2017-03-06 19:22:24 +0100

    Project initialisation

---
+++ src/main/java/fr/isima/RetroGamingStudioApplication.java
@@ -0,0 +1,13 @@
+package fr.isima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroGamingStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroGamingStudioApplication.class, args);
+	}
+
+}
